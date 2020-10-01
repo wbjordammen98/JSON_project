f594eda1e53bd6d24c5a475e0e891e2f1f066a9e
wbjordammen98 <wbjordammen@gmail.com>
2020-09-30 21:12:28 -0500

    inital check in

--- explore_json_5.py
+++ explore_json_5.py
@@ -1,7 +1,9 @@
 import json
 
+# Opens json file for fire data to be read in the program.
 in_file = open('US_fires_9_14.json','r')
 
+# Opens out file for the exact data we want to pull from the file. 
 out_file = open('readable_fire_data_9_14.json','w')
 
 fire_data = json.load(in_file)
@@ -14,8 +16,11 @@ print(type(list_of_fires))
 
 print(len(list_of_fires))
 
+# Create empty lists for the brightness level and coordinates of each location in the data file.
 brights,lons,lats,hover_texts = [],[],[],[]
 
+# Runs through each location and appends the brightness factor and coordinates of each location that has a brightness factor greater
+# than 450.
 for f in list_of_fires:
     bright = f['brightness']
     lon = f['longitude']
@@ -35,9 +40,11 @@ print(lons[:10])
 print('Lats')
 print(lats[:10])
 
+# Import plotly to plot map of the locations appended. 
 from plotly.graph_objs import Scattergeo, Layout
 from plotly import offline
 
+# Creates the map of the locations plotted.
 data = [Scattergeo(lon=lons, lat=lats)]
 
 data = [{
